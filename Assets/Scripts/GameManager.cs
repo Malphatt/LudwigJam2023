79f1169e8e36cc0d08c97de250634ca21b05fd0d
Alex Dickinson <P215280@outlook.com>
2023-02-17 03:45:08 +0000

    Added Menus
    fixes stuff

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public GameObject timer;
+    public float accumulatedTime;
+    
     // Update is called once per frame
     void Update()
     {
-        
+        accumulatedTime += Time.deltaTime;
+        //Format time in 00:00:00:00
+        timer.GetComponent<TextMeshProUGUI>().text = string.Format("{0:00}:{1:00}:{2:00}", accumulatedTime / 3600, (accumulatedTime % 3600) / 60, (accumulatedTime % 60));
     }
 }
